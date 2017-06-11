760f5081af5a35836215629950d717d867881b85
Shantih Etzi <shantih19@gmail.com>
2017-06-12 01:05:26 +0200

    Updated Alert and added regex to Parser

--- alert.py
+++ alert.py
@@ -2,11 +2,11 @@ import telepot
 import sqlite3
 import sys
 def sendAll(text, bot, cr):
-    cr.execute("SELECT id FROM users;")
+    cr.execute("SELECT id FROM users where id = 129434903;")
     users = cr.fetchall()
     print(users)
-    for i in users[0]:
-        bot.sendMessage(i, text)
+    for i in users:
+        bot.sendMessage(users[users.index(i)][0], text)
         
 def panic(text, bot, cr):
     sendAll(text,bot,cr)
--- parser.py
+++ parser.py
@@ -2,24 +2,24 @@ import subprocess
 import sqlite3
 import random
 import emoji
-
+import re
 def parseText(message, status, name, id, db):
     cr = db.cursor()
-    if 'ciao' in message or 'hey' in message or 'heila' in message or 'salve' in message:
+    if re.search('ci+a+o+', message) is not None or re.search('he+y+', message) is not None or re.search('he+i+la+', message) is not None or re.search('sa+lve+', message) is not None:
         if status == 'neutral':
             return random.choice(['Ciao', 'Heila', 'Hey', 'Salve', 'Hola', 'Buonsalve']) + ' ' + name.lower()
         elif status == 'angry':
             return random.choice(['...', 'Cazzo vuoi?'])
-    if 'come va?' in message or 'come stai?' in message:
+    if re.search('come va+', message) is not None or re.search('come stai+', message) is not None:
         if status == 'neutral':
             return random.choice(['Bene dai uwu', 'Non male uwu', 'Come sempre uwu'])
         elif status == 'angry':
             return random.choice(['Non sono cazzi tuoi.', 'Che ti frega a te?', 'Non rompere.', '...'])
-    if message == 'giada':
+    if re.match('giada+', message) is not None:
         return emoji.emojize(':smiling_face_with_heart-eyes::smiling_face_with_heart-eyes::smiling_face_with_heart-eyes::smiling_face_with_heart-eyes::smiling_face_with_heart-eyes:')
     if message == 'temp':
         return subprocess.check_output(['/opt/vc/bin/vcgencmd', 'measure_temp'])
-    if 'che fai' in message or 'cosa fai' in message:
+    if re.search('che+ fa+i+', message) is not None or re.search('co+sa+ fa+i+', message) is not None:
         cr.execute("SELECT count(*) FROM users;")
         users =str(cr.fetchone()[0])
         if status == 'neutral':
@@ -30,9 +30,9 @@ def parseText(message, status, name, id, db):
         cr.execute("UPDATE users SET status = 'angry' WHERE id = ?;", [id])
         db.commit()
         return random.choice(['Ma vacci te.', '...', 'Ti odio', 'Ma che cazzo vuoi?', 'Ma.'])
-    if 'come ti chiami?' in message:
+    if 'come ti chiami' in message:
         return 'Berry. Rasp-Berry. Il mio creatore è originale quanto ' + random.choice(['una calza ', 'una marmotta ', 'una marmitta ', 'una barca a vela ']) + random.choice(['ubriaca.', 'morta.', 'in fiamme.' , 'a rotelle'])
-    if 'scusa' in message or 'perdonami' in message:
+    if re.search('scu+sa+', message) is not None or re.search('pe+rdo+na+mi+', message) is not None:
         cr.execute("UPDATE users SET status = 'neutral' WHERE id = ?;" , [id])
         db.commit()
         return random.choice(['Okay uwu', 'Sei perdonato per stavolta', 'Si figuri milord'])
